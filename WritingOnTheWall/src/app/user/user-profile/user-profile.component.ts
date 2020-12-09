3f11415cfe93c76a9529447a888e618d508a54df
bescoto98 <escoto.brenda42@gmail.com>
2020-12-09 11:03:33 -0800

    very ugly user profile page added

--- WritingOnTheWall/src/app/user/user-profile/user-profile.component.ts
+++ WritingOnTheWall/src/app/user/user-profile/user-profile.component.ts
@@ -28,6 +28,11 @@ export class UserProfileComponent implements OnInit {
     {"media":"The Song of Achilles","opinion":"the greeks would hate this which makes it even better"}
   ];
 
+  fakeLists = [
+    {"title":"knightcore aesthetic","description":"it's about the chivalry. and also i want a big sword to defeat my enemies hehe"},
+    {"title":"scifi movies better than star wars","description":"which isn't really a high bar when you think about it"},
+    {"title":"the epic highs and lows of superhero media","description":"yes i think dc sucks yes i'm correct"}
+  ];
   constructor() { }
 
   ngOnInit(): void {
