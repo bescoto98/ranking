ea704f73af6e1dafbad40e9a5fed8d5c7cd0e916
bescoto98 <escoto.brenda42@gmail.com>
2020-12-09 14:12:27 -0800

    trying to make profile page nicer

--- WritingOnTheWall/src/app/home/register/register.component.ts
+++ WritingOnTheWall/src/app/home/register/register.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  index: number = 1;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  moveOn(){
+    this.index++;
   }
 
 }
