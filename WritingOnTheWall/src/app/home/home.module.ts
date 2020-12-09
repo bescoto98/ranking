55a1c19e13b65719ef3c382e873e730755f47d6f
bescoto98 <escoto.brenda42@gmail.com>
2020-12-09 10:33:32 -0800

    working on user profile

--- WritingOnTheWall/src/app/home/home.module.ts
+++ WritingOnTheWall/src/app/home/home.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomePageComponent } from './home-page/home-page.component';
 
-import { NavbarModule } from '@navbar/navbar.module';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { HomeRootComponent } from './home-root/home-root.component';
@@ -14,8 +13,7 @@ import { HomeRootComponent } from './home-root/home-root.component';
   declarations: [HomePageComponent, RegisterComponent, LoginComponent, HomeRootComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule,
-    NavbarModule
+    HomeRoutingModule
   ]
 })
 export class HomeModule { }
--- WritingOnTheWall/src/app/user/user-profile/user-profile.component.ts
+++ WritingOnTheWall/src/app/user/user-profile/user-profile.component.ts
@@ -7,9 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
+  showList: boolean = false;
+  listButton: string = "Expand list";
+
+  reviews = [
+      { "media":"Harry Potter" , "opinion":"this sucked", "agree":54, "disagree":2 },
+      { "media":"Twilight", "opinion":"effervescent", "agree":5, "disagree":3 }
+  ];
+
+  topten = [
+    {"media":"Dragonheart", "opinion":"invented romance as we know it"},
+    {"media":"High School Musical", "opinion":"full of absolute bangers"},
+    {"media":"Last Knights","opinion":"clive owen looking sexy"},
+    {"media":"Tron: Legacy","opinion":"daft punk killing it for two hours straight"},
+    {"media":"The 100","opinion":"its so bad that it has unfortunately become good"},
+    {"media":"Power Rangers: Mystic Force","opinion":"formative experience"},
+    {"media":"Nimona","opinion":"gay people <3"},
+    {"media":"Revenge of the Sith novelization","opinion":"also a formative experience"},
+    {"media":"Pacific Rim","opinion":"the only good thing del toro has made"},
+    {"media":"The Song of Achilles","opinion":"the greeks would hate this which makes it even better"}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleList(){
+    this.showList = !this.showList;
+
+    if(this.showList){
+      this.listButton = "Collapse list";
+    }
+    else{
+      this.listButton = "Expand list";
+    }
   }
 
 }
